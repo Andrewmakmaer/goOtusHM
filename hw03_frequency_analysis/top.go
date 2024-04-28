d0b9de066c23364018f1e73aa16bec97e095f1dc
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-04-28 13:22:35 +0300

    feature: solve HW03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,50 @@
+// package main
+
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+	"unicode"
+)
+
+func Top10(st string) []string {
+	wordCouter := make(map[string]int)
+	formatedString := strings.ReplaceAll(st, "\n", " ")
+	stSlice := strings.Split(formatedString, " ")
+	for _, item := range stSlice {
+		itemWord := strings.TrimSpace(item)
+		itemWord = strings.ToLower(itemWord)
+		itemWord = strings.TrimFunc(itemWord, func(r rune) bool {
+			return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+		})
+
+		if itemWord == "" {
+			continue
+		}
+		value, ok := wordCouter[itemWord]
+		if ok {
+			wordCouter[itemWord] = value + 1
+		} else {
+			wordCouter[itemWord] = 1
+		}
+	}
+	words := make([]string, 0, len(wordCouter))
+
+	for word := range wordCouter {
+		words = append(words, word)
+	}
+
+	sort.SliceStable(words, func(i, j int) bool {
+		if wordCouter[words[i]] != wordCouter[words[j]] {
+			return wordCouter[words[i]] > wordCouter[words[j]]
+		}
+		return words[i] < words[j]
+	})
+
+	if len(words) > 10 {
+		return words[:10]
+	}
+
+	return words
 }
