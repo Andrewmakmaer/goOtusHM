fa83d50f7595bc9ade986965d5c45cc08138a2fc
Andrey Mitichev <Andrey-Mitichev@KK-Huawei.local>
2024-05-03 08:33:15 +0300

    release list

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,80 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
+	// List Remove me after realization.
 	// Place your code here.
+	firstNode  *ListItem
+	lastNode   *ListItem
+	lenghtList int
+}
+
+func (l *list) Len() int {
+	return l.lenghtList
+}
+
+func (l *list) Front() *ListItem {
+	return l.firstNode
+}
+
+func (l *list) Back() *ListItem {
+	return l.lastNode
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newListItem := new(ListItem)
+	newListItem.Value = v
+	if l.Len() == 0 {
+		l.firstNode = newListItem
+		l.lastNode = newListItem
+	} else {
+		newListItem.Next = l.firstNode
+		l.firstNode.Prev = newListItem
+		l.firstNode = newListItem
+	}
+	l.lenghtList = l.lenghtList + 1
+	return newListItem
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	newListItem := new(ListItem)
+	newListItem.Value = v
+	if l.Len() == 0 {
+		l.firstNode = newListItem
+		l.lastNode = newListItem
+	} else {
+		newListItem.Prev = l.lastNode
+		l.lastNode.Next = newListItem
+		l.lastNode = newListItem
+	}
+	l.lenghtList = l.lenghtList + 1
+	return newListItem
+}
+
+func (l *list) Remove(item *ListItem) {
+	left := item.Prev
+	right := item.Next
+
+	if left != nil {
+		left.Next = right
+	} else {
+		l.firstNode = right
+	}
+
+	if right != nil {
+		right.Prev = left
+	} else {
+		l.lastNode = left
+	}
+
+	l.lenghtList = l.lenghtList - 1
+}
+
+func (l *list) MoveToFront(item *ListItem) {
+	if item.Prev != nil {
+		// itemCopyValue := item.Value
+		l.PushFront(item.Value)
+		l.Remove(item)
+	}
 }
 
 func NewList() List {
