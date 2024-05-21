1dba7587c02c4b508073734cfb3a37751c793066
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-05-21 11:02:23 +0300

    add test for 0 error count

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -112,4 +112,27 @@ func TestRun(t *testing.T) {
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.Falsef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 	})
+
+	t.Run("zero errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		maxErrorsCount := 0
+		err := Run(tasks, workersCount, maxErrorsCount)
+
+		require.Truef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
+		require.LessOrEqual(t, int32(workersCount+maxErrorsCount), runTasksCount, "extra tasks were started")
+	})
 }
