d7c38d304224b4d91d5e6fec7907bd53dca8139c
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-05-21 10:05:46 +0300

    task 5

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"runtime"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -66,5 +68,48 @@ func TestRun(t *testing.T) {
 
 		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
 		require.LessOrEqual(t, int64(elapsedTime), int64(sumTime/2), "tasks were run sequentially?")
+	})
+
+	t.Run("Try run without sleep", func(t *testing.T) {
+		tasksCount := 500
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			var result error
+			if i%10 == 0 {
+				result = fmt.Errorf("error from task %d", i)
+			} else {
+				result = nil
+			}
+			tasks = append(tasks, func() error {
+				time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
+				atomic.AddInt32(&runTasksCount, 1)
+				return result
+			})
+		}
+
+		workersCount := 20
+		maxErrorsCount := 51
+		var countGorutines int
+		var wg sync.WaitGroup
+		var err error
+		wg.Add(2)
+		go func() {
+			err = Run(tasks, workersCount, maxErrorsCount)
+			wg.Done()
+		}()
+		go func() {
+			time.Sleep(time.Millisecond * time.Duration(100))
+			countGorutines = runtime.NumGoroutine() // надо как-то сделать чтобы показатель снимался во время рана основного
+			wg.Done()
+		}()
+		wg.Wait()
+
+		require.Equal(t, workersCount, countGorutines-5, "Max gorutines count")
+		// countGorutines-5 т.к. есть горутины запускаемые не только в Run функции
+		require.Equal(t, runTasksCount, int32(tasksCount), "not all tasks were completed")
+		require.Falsef(t, errors.Is(err, ErrErrorsLimitExceeded), "actual err - %v", err)
 	})
 }
