cba54bf0f1ec6b63436eb005bf76b988f936b54a
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-05-26 20:17:22 +0300

    Start writing

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,19 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	outChannel := make(Bi)
+	for _, stage := range stages {
+		in = stage(in)
+	}
+	go func() {
+		defer close(outChannel)
+		for i := range in {
+			select {
+			case <-done:
+				return
+			case outChannel <- i:
+			}
+		}
+	}()
+	return outChannel
 }
