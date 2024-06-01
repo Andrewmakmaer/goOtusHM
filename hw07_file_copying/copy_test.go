1a149b3ca4837841b3acbe66a3bfc082853b5034
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-06-01 12:02:52 +0300

    solve hw07

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,26 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Copy non exist file", func(t *testing.T) {
+		err := Copy("nonexist.txt", "nonexist.txt.copy", 0, 0)
+		require.Truef(t, errors.Is(err, os.ErrNotExist), "actual error %q", err)
+	})
+
+	t.Run("offset is larger than file size", func(t *testing.T) {
+		err := Copy("testdata/smallfile.txt", "small.txt.copy", 10, 0)
+		require.Truef(t, errors.Is(err, ErrOffsetExceedsFileSize), "actual error %q", err)
+	})
+
+	t.Run("offset is larger than file size", func(t *testing.T) {
+		err := Copy("/dev/urandom", "small.txt.copy", 10, 0)
+		require.Truef(t, errors.Is(err, ErrUnsupportedFile), "actual error %q", err)
+	})
 }
