67634a8e8f839ac523de57ea70094929fb7787de
Andrew Makmaer <andrewmakmaer@gmail.com>
2024-07-09 10:55:57 +0300

    fix

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -2,7 +2,6 @@ package hw10programoptimization
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"strings"
 	"sync"
@@ -32,43 +31,24 @@ func (us *User) Reset() {
 	us.Address = ""
 }
 
-func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
-	if err != nil {
-		return nil, fmt.Errorf("get users error: %w", err)
-	}
-	return countDomains(u, domain)
-}
-
-type users [100_000]User
-
 var userPool = sync.Pool{
 	New: func() interface{} { return &User{} },
 }
 
-func getUsers(r io.Reader) (result users, err error) {
+func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	scanner := bufio.NewScanner(r)
-	counter := 0
+	result := make(DomainStat)
+
 	for scanner.Scan() {
 		user := userPool.Get().(*User)
 		user.Reset()
-		if err = jsoniter.Unmarshal(scanner.Bytes(), user); err != nil {
+
+		if err := jsoniter.Unmarshal(scanner.Bytes(), user); err != nil {
 			userPool.Put(user)
-			return
+			return nil, err
 		}
-		result[counter] = *user
-		userPool.Put(user)
-		counter += 1
-	}
-	return
-}
 
-func countDomains(u users, domain string) (DomainStat, error) {
-	result := make(DomainStat)
-
-	for _, user := range u {
 		matched := strings.HasSuffix(user.Email, "."+domain)
-
 		if matched {
 			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
 			num++
